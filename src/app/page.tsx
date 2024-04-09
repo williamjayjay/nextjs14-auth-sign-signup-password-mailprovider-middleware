b357bc8777a84b0c7f96011a1a9e6e268531b0ea
williamjayjay <williamdn@yahoo.com>
2024-04-09 18:41:17 -0300

    feat: send email smtp with google

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+import { sendMail } from "@/lib/mail";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+
+  await sendMail({
+    to: "youremail@gmail.com",
+    subject: "Teste NextJS",
+    body: "testando...."
+  })
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
